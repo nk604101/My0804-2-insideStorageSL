157cde8ae9e2b8421d214364a58ce49505b78022
HsinWei <fatjuan2000@hotmail.com>
2016-08-04 15:13:18 +0800

    Json file save

--- app/src/main/java/com/work/hsinwei/my0804_2_insidestoragesl/MainActivity.java
+++ app/src/main/java/com/work/hsinwei/my0804_2_insidestoragesl/MainActivity.java
@@ -2,6 +2,19 @@ package com.work.hsinwei.my0804_2_insidestoragesl;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +22,78 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+        File f = getCacheDir();
+        Log.d("T0804-File",f.toString());
+        String fName = "mydata";
+        FileOutputStream fOut = null;
+        try {
+            //儲存檔案
+            fOut=openFileOutput(fName,MODE_PRIVATE);
+            OutputStreamWriter osw = new OutputStreamWriter(fOut);
+            osw.write("測試輸入資料");
+            osw.close();
+            //讀取檔案
+            File readFile = new File(getFilesDir()+File.separator+fName);
+            char[] buffer = new char[1];
+            FileReader fr = null;
+            StringBuilder sb = new StringBuilder();
+
+            fr= new FileReader(readFile);
+
+            Log.d("T0804-Char",""+buffer);
+            Log.d("T0804-Char",""+buffer[0]);
+
+            while (fr.read(buffer) != -1)
+            {
+                sb.append(new String(buffer));
+            }
+            fr.close();
+            Log.d("T0804-FileR",sb.toString());
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    //Json 資料轉檔存入
+    public void click1(View v)
+    {
+        String fName = "mydata.Json";
+        FileOutputStream fOut = null;
+        JSONArray array= new JSONArray();
+        JSONObject obj;
+        try {
+            obj=new JSONObject();
+            obj.put("name","名字");
+            obj.put("addr","地址");
+            obj.put("tel","電話");
+            array.put(obj);
+
+            obj=new JSONObject();
+            obj.put("name","名字2");
+            obj.put("addr","地址2");
+            obj.put("tel","電話2");
+            //Log.d("T0804-JsonS",obj.toString());
+            array.put(obj);
+            Log.d("T0804-JsonS",array.toString());
+
+            fOut=openFileOutput(fName,MODE_PRIVATE);
+            OutputStreamWriter osw = new OutputStreamWriter(fOut);
+            osw.write(array.toString());
+            osw.close();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    //Gson
+    public void click2(View v)
+    {
+
     }
 }
